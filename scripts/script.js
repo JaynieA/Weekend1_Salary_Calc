f22559414892b465df3b1ce7ca60217f3224bab9
Jaynie <mrs.jaynie.anderson@gmail.com>
2016-11-23 18:38:11 -0600

    added onclick to submit button, prevented refresh

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 console.log('sourcing scripts.js');
+
+//input field id's:
+//firstName, lastName, idNum, jobTitle, annualSalary
+
+var Employee = function(firstName, lastName, idNum, jobTitle, annualSalary) {
+  console.log('in Employee: '+ firstName, lastName, idNum, jobTitle, annualSalary);
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.idNum = idNum;
+  this.jobTitle = jobTitle;
+  this.annualSalary = annualSalary;
+}; // end Employee constructor
+
+var getUserInput = function() {
+  alert('In getUserInput');
+  return false;
+}; //end getUserInput
+
+// var me = new Employee("Jaynie", "Anderson", '1', 'Boss Lady', 100000000);
+//
+// console.log(me);
