7c59ea8b95f8701ac2e1544c060d837ab021be36
Jaynie <mrs.jaynie.anderson@gmail.com>
2016-11-26 13:55:07 -0500

    Displaying total monthly salary cost

--- scripts/script.js
+++ scripts/script.js
@@ -25,12 +25,12 @@ var getUserInput = function() {
   //clear input values and add focus to first input to mimick refresh
   $('input').val('');
   $('#firstName').focus();
-
+  //call functions to append employees to DOM and calculate monthy salary cost
   appendEmployees();
+  calcSalaryCost();
   //prevent page refresh by returning false,
   return false;
 }; //end getUserInput
-
 
 var appendEmployees = function() {
   console.log('in appendEmployees');
@@ -38,5 +38,18 @@ var appendEmployees = function() {
   var index = Number(employees.length)-1;
   var newEmployeeRow = '<tr><td>'+employees[index].firstName+'</td><td>'+employees[index].lastName+'</td><td>'+employees[index].idNum+'</td><td>'+employees[index].jobTitle+'</td><td>'+employees[index].annualSalary+'</td></tr>';
   $('#employeeTable').append(newEmployeeRow);
-
 }; //end appendEmployees
+
+var calcSalaryCost = function(){
+  console.log('in calcSalaryCost');
+  salaryCost=0;
+  //add up the salary of each employee
+  for (var i = 0; i < employees.length; i++) {
+    salaryCost += Number(employees[i].annualSalary);
+  }
+  console.log(salaryCost/12);
+  $('.salaryCost').show();
+  //display total monthly salary cost on DOM
+  $('#salaryTotal').html(salaryCost/12);
+  return salaryCost;
+}; //end calcSalaryCost
