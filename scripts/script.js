7cdfe27a6fea9226b69affaac575f0d328dacf30
Jaynie <mrs.jaynie.anderson@gmail.com>
2016-11-26 12:30:29 -0500

    Recieved input from user on submit, appended to styled table on DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
-console.log('sourcing scripts.js');
-
-//input field id's:
-//firstName, lastName, idNum, jobTitle, annualSalary
-
 var Employee = function(firstName, lastName, idNum, jobTitle, annualSalary) {
   console.log('in Employee: '+ firstName, lastName, idNum, jobTitle, annualSalary);
   this.firstName = firstName;
@@ -12,11 +7,36 @@ var Employee = function(firstName, lastName, idNum, jobTitle, annualSalary) {
   this.annualSalary = annualSalary;
 }; // end Employee constructor
 
+//declare employees array
+var employees = [];
+
 var getUserInput = function() {
-  alert('In getUserInput');
+  console.log('In getUserInput');
+  //get user input variables
+  var firstName = $('#firstName').val();
+  var lastName = $('#lastName').val();
+  var idNum = $('#idNum').val();
+  var jobTitle = $('#jobTitle').val();
+  var annualSalary = $('#annualSalary').val();
+  console.log(firstName, lastName, idNum, jobTitle, annualSalary);
+  //create new employee and push it to the employees array
+  employees.push(new Employee (firstName, lastName, idNum, jobTitle, annualSalary));
+  console.log(employees);
+  //clear input values and add focus to first input to mimick refresh
+  $('input').val('');
+  $('#firstName').focus();
+
+  appendEmployees();
+  //prevent page refresh by returning false,
   return false;
 }; //end getUserInput
 
-// var me = new Employee("Jaynie", "Anderson", '1', 'Boss Lady', 100000000);
-//
-// console.log(me);
+
+var appendEmployees = function() {
+  console.log('in appendEmployees');
+  //append employees array to the DOM
+  var index = Number(employees.length)-1;
+  var newEmployeeRow = '<tr><td>'+employees[index].firstName+'</td><td>'+employees[index].lastName+'</td><td>'+employees[index].idNum+'</td><td>'+employees[index].jobTitle+'</td><td>'+employees[index].annualSalary+'</td></tr>';
+  $('#employeeTable').append(newEmployeeRow);
+
+}; //end appendEmployees
